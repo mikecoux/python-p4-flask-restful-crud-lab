b3ae7187dbc7c5bc1469a6ba005d36e133a6226e
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-05-24 12:43:05 -0600

    completed

--- server/app.py
+++ server/app.py
@@ -40,10 +40,28 @@ class Plants(Resource):
 api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        for attr in request.get_json():
+            setattr(plant, attr, request.get_json().get(attr))
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
